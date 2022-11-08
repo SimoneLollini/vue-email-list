c3a0858bbc07a50f62b6b5e21f52670704186baf
SimoneLollini <lollinisimone@gmail.com>
2022-11-08 16:08:10 +0100

    Add emailsList

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,30 +10,32 @@
 
 /*CDN da utilizzare nell head dell documento html IMPORTANTE: va aggiunto l'attributo defer
 https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js)*/
-
+const emailsList = []
 const { createApp } = Vue
 
 createApp({
     data() {
         return {
             message: 'Hello Vue!',
-
+            email: null,
+            emailsList
         }
     },
-    mounthed() {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(
-
-        )
+    mounted() {
+        for (let index = 0; index < 10; index++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => {
+                    // console.log(response.data.response);
+                    this.email = response.data.response
+                    this.emailsList.push(this.email)
+                });
+        }
     }
 }).mount('#app')
 
 //dovro utilizzare un ciclo for del tipo
 /*
-for (let index = 0; index < 10; index++) {
-
-    
-}
 */
 
 
